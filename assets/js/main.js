1600c9b4a5f5c764833849a3961d5c2369b28c91
nair0lf32 <florianedem@gmail.com>
2021-10-11 15:12:35 +0100

    rename js file and fix sw path

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
+// Only trigger if service workers are supported in browser.
 if ('serviceWorker' in navigator) {
     // Wait until window is loaded before registering.
     window.addEventListener('load', () => {
-        // Register the service worker
-        navigator.serviceWorker.register("{{ '/service-worker.js?v=' | append: site.github.build_revision | relative_url }}")
+        // Register the service worker with "/" as it's scope.
+        navigator.serviceWorker.register("service-worker.js", { scope: '/' })
             // Output success/failure of registration.
             .then(() => console.log('SW registered'))
             .catch(() => console.error('SW registration failed'));
