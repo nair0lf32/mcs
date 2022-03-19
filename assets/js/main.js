5d169914d94976e2b14bad0c9d546d729305110f
nair0lf32 <florianedem@gmail.com>
2022-03-19 15:04:17 +0100

    fix service worker registration error

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@
 if ('serviceWorker' in navigator) {
     // Wait until window is loaded before registering.
     window.addEventListener('load', () => {
-        // Register the service worker with "/" as it's scope.
-        navigator.serviceWorker.register("service-worker.js", { scope: '/' })
-            // Output success/failure of registration.
-            .then(() => console.log('SW registered'))
-            .catch(() => console.error('SW registration failed'));
+        // Register the service worker with default scope.
+        navigator.serviceWorker.register("./service-worker.js")
+            // Debugging.
+            .then(() => console.log('SW registered!'))
+            .catch((error) => console.error('SW registration failed!', error));
     });
 }
