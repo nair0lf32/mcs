7d09cf20d00369b92885fc26c2af8b2460418015
nair0lf32 <florianedem@gmail.com>
2022-03-19 15:55:37 +0100

    minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
     // Wait until window is loaded before registering.
     window.addEventListener('load', () => {
         // Register the service worker with default scope.
-        navigator.serviceWorker.register("./service-worker.js")
+        navigator.serviceWorker.register("/service-worker.js")
             // Debugging.
             .then(() => console.log('SW registered!'))
             .catch((error) => console.error('SW registration failed!', error));
