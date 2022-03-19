7461fe648ec62a2e645023e4c69b44faf3f71cba
nair0lf32 <florianedem@gmail.com>
2022-03-19 16:20:16 +0100

    fix for pwa scope

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
     // Wait until window is loaded before registering.
     window.addEventListener('load', () => {
         // Register the service worker with base scope.
-        navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+        navigator.serviceWorker.register('./service-worker.js', { scope: './' })
             // Debugging.
             .then(() => console.log('SW registered!'))
             .catch((error) => console.error('SW registration failed!', error));
