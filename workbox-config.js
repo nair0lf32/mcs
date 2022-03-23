c3238e83d2aa7303c98401d92622267c31ee3691
nair0lf32 <florianedem@gmail.com>
2022-03-23 16:12:53 +0100

    minor changes

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,12 @@
 module.exports = {
 	globDirectory: '_site/',
 	globPatterns: [
-		'**/*.{html,css,png,ico,db,js,jpg,webp,pdf,xml,json,md}'
+		'**/*.{html,css,scss,png,ico,db,js,jpg,webp,pdf,xml,json,markdown,md}'
 	],
 	swDest: 'service-worker.js',
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
-	]
+	],
 };
+
